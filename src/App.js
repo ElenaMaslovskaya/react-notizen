d4b703988f0984872cb1cbc9155c94d3b857755f
Elena Maslovskaya <elena.maslovski@yandex.ru>
2022-03-02 16:54:49 +0100

    components, styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './styles/App.css';
 
 function App() {
+
+  const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="post">
+        <div className=".post__content">
+          <strong>1. Javascript</strong>
+          <div>
+            Javascript - язык программирования
+          </div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+        </div>
+      </div>
     </div>
   );
 }

359f8f2146291d29acb023e999d7d7e99441e8d3
Elena Maslovskaya <elena.maslovski@yandex.ru>
2022-03-04 15:29:25 +0100

    PostItem, PostList, MyButton, MyInput

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React, { useState } from "react";
+import PostList from "./components/PostList";
 import './styles/App.css';
+import MyButton from "./UI/button/MyButton";
+import MyInput from "./UI/input/MyInput";
 
 function App() {
 
-  const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ');
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Javascript 2', body: 'Description' },
+    { id: 3, title: 'Javascript 3', body: 'Description' }
+  ]);
+
+  const [title, setTitle] = useState('');
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+  };
+
   return (
     <div className="App">
-      <div className="post">
-        <div className=".post__content">
-          <strong>1. Javascript</strong>
-          <div>
-            Javascript - язык программирования
-          </div>
-        </div>
-        <div className="post__btns">
-          <button>Удалить</button>
-        </div>
-      </div>
+      <form>
+        <MyInput
+        value={title} 
+        type="text" 
+        placeholder="Название поста" 
+        onChange={(e => setTitle(e.target.value))}
+        />
+        <MyInput type="text" placeholder="Описание поста" />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title={'Посты про JS'} />
     </div>
   );
 }
